342e86d111dce5924942daae297ff0fede63a900
LGoulart <leandrovgoulart@gmail.com>
2023-10-07 19:42:59 -0300

    update main js

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+const botoes = document.querySelectorAll('.botao');
+
+const personagens = document.querySelectorAll('.personagem');
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        const botaoSelecionado = document.querySelector('.botao.selecionado');
+        botao.classList.add('selecionado');
+
+        const personagemSelecionado = document.querySelector('.personagem.selecionado');
+        personagemSelecionado.classList.remove('selecionado');
+        personagens[indice].classList.add('selecionado');
+    });
+});
+
+ 
+ 
