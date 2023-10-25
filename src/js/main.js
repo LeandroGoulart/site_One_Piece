184b75605edebf639d5072b110214fa11caa6563
LGoulart <leandrovgoulart@gmail.com>
2023-10-25 16:31:09 -0300

    corrigido bug de remover seleção

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll('.botao');
-
 const personagens = document.querySelectorAll('.personagem');
 
 botoes.forEach((botao, indice) => {
     botao.addEventListener('click', () => {
         const botaoSelecionado = document.querySelector('.botao.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
         botao.classList.add('selecionado');
 
         const personagemSelecionado = document.querySelector('.personagem.selecionado');
@@ -12,6 +12,4 @@ botoes.forEach((botao, indice) => {
         personagens[indice].classList.add('selecionado');
     });
 });
-
- 
  
